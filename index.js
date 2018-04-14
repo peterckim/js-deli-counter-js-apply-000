78646f068b8750581bf56013a90a5facc873984d
Peter Kim <peterckim96@gmail.com>
2018-04-14 20:14:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing() {
-  
+function nowServing(line) {
+  if (line.length === 0) {
+    return 'There is nobody waiting to be served!';
+  }
+  if (line.length > 0) {
+    var first
+  }
 }
